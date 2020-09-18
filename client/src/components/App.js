6b4329d8559bd298443ea697b8ac348c11ffd9d5
Wes Hightower <bundleofstrings@gmail.com>
2020-09-17 23:19:10 -0500

    Restoring main functionality I hope.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,3 @@
-// initial view; splash screen
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -18,7 +17,7 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <div className="container">
+          <div>
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
